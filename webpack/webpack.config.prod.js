289c111cc6c0b47a234ba34ad6f0a5010f864f19
xupoplar <xupoplar@gmail.com>
2019-05-27 22:14:54 -0500

    Add autoprefixer

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -31,10 +31,11 @@ module.exports = merge(common, {
       },
       {
         test: /\.s?css/i,
-        use : [
+        use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'sass-loader'
+          'postcss-loader',
+          'sass-loader',
         ]
       }
     ]
